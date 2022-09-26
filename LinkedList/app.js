055bf30556361341d2a4b233b49569f30d1b14e9
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2022-09-26 14:34:10 -0700

    new design

---
+++ LinkedList/app.js
@@ -0,0 +1,8 @@
+// const Linkedlist = require('./LinkedList')
+
+// const ll = new Linkedlist.fromValues(10, 20, 30, 40)
+// ll.print()
+// console.log(ll.getByIndex(0).value)
+
+// ll.insertAtIndex(2, 60)
+// ll.print();
